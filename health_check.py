adf8351ae9ae522401d17a5889e0d4d5d4aab507
4801f421 <4801f4214lin32had@gmail.com>
2024-12-30 00:24:13 +0330

    feat(helth check): check if config.json exict

--- health_check.py
+++ health_check.py
@@ -2,6 +2,7 @@ import os
 import socket
 
 LOG_FILE_PATH = "/marzneshiniplimitcode/app.log"
+CONFIG_FILE_PATH = "/marzneshiniplimitcode/config.json"
 
 def check_logs_for_errors():
     """بررسی لاگ برای متن 'Unexpected error'"""
@@ -17,15 +18,22 @@ def check_logs_for_errors():
         return False, f"Error reading log file: {str(e)}"
     return True, "Logs are clean"
 
+def check_config_file():
+    """بررسی وجود فایل config.json"""
+    if os.path.exists(CONFIG_FILE_PATH):
+        return True, f"Config file exists: {CONFIG_FILE_PATH}"
+    return False, f"Config file not found: {CONFIG_FILE_PATH}"
+
 def run_health_checks():
     checks = [
         check_logs_for_errors,
+        check_config_file
     ]
     for check in checks:
         result, message = check()
         if not result:
             return False, message
-    return True, "Health check passed"
+    return True, "All health checks passed"
 
 if __name__ == "__main__":
     health_status, message = run_health_checks()
